dbc85a1babc84c93a1d9f29af4bb5e479619cdd1
CozlovschiNichita <www.nik@icloud.com>
2025-05-14 00:46:36 +0300

    errorMsg

--- js/main.js
+++ js/main.js
@@ -2,17 +2,18 @@
 
 const companyInput = document.getElementById('companyInput');
 const startBtn     = document.getElementById('startButton');
+const errorMsg     = document.getElementById('errorMsg');
 
 startBtn.addEventListener('click', () => {
   const name = companyInput.value.trim();
 
   if (!name) {
-    alert('Пожалуйста, введите название компании');
+    errorMsg.style.display = "block";   
     companyInput.focus();
     return;
   }
 
+  errorMsg.style.display = "none";        
   localStorage.setItem('companyName', name);
-
   window.location.href = 'form1.html';
 });
